e415c5aa5ceaf3444f19e637db1243e830a65531
Joseph Finlayson <joseph.finlayson@gmail.com>
2021-02-10 19:00:49 +0100

    add better tests

--- test_app.py
+++ test_app.py
@@ -1,5 +1,17 @@
 from datetime import datetime
-from app import is_golden_hour, index
+from routes import is_golden_hour, index, post_story, app
+import pytest
+import werkzeug
+import io
+import base64
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+
+    with app.test_client() as client:
+        yield client
+
 
 def test_if_golden_hour_test():
 
@@ -9,8 +21,29 @@ def test_if_golden_hour_test():
 
     assert is_golden_hour(now, sunset, sunrise) == True
 
-def test_index_test():
-    answer = index()
+def test_index_test(client):
+    answer = client.get('/').json
+    print(answer)
     g = list(answer).pop()
 
     assert g == 'golden_hour'
+
+SMALLEST_JPEG_B64 = """\
+/9j/2wBDAAMCAgICAgMCAgIDAwMDBAYEBAQEBAgGBgUGCQgKCgkICQkKDA8MCgsOCwkJDRENDg8Q
+EBEQCgwSExIQEw8QEBD/yQALCAABAAEBAREA/8wABgAQEAX/2gAIAQEAAD8A0s8g/9k=
+"""
+
+file = werkzeug.datastructures.FileStorage(
+        stream=io.BytesIO(base64.b64decode(SMALLEST_JPEG_B64)),
+        filename="example image.jpg",
+        content_type="image/jpg",
+    )
+
+def test_image_to_video(client):
+    f = open('./sample.jpg', 'rb') 
+    f2 = open('./sample_2.jpg', 'rb') 
+    f.seek(0)
+    f2.seek(0)
+
+    response = client.post('/post-story', data=dict(image =[f, f2]))
+    assert response.headers['Content-Type'] == 'video/mp4'
